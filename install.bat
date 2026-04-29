0f0586cb3635c89a4d5a5209b1f61f38267f4e96
Keiitaii <liqitai70@gmail.com>
2026-04-29 22:12:23 +0900

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Double-click this to install voicebridge shortcuts (autostart + desktop + Start menu).
-REM Requires Python 3.12+ on PATH and `pip install pywin32` already done.
-
-cd /d "%~dp0"
-python autostart.py install-all
-echo.
-pause
